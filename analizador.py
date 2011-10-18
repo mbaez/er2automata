d02594d2df313f7826dc67fd59633eadead9453a
Maximiliano Báez González <mxbg.py@gmail.com>
2011-10-18 18:32:54 -0300

    Se añadieron comentarios a los metodos

--- analizador.py
+++ analizador.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 from keys import *
 from string_tokenizer import *
+"""
+:author: Maximiniliano Báez González
+:contact: mxbg.py@gmail.com
+"""
 
+#Variable global que define si se mostran los detalles del proceso
 DEBUG = False
 
 class Analizador:
@@ -33,6 +38,13 @@ class Analizador:
         self.postfija = []
 
     def start(self):
+        """
+        Este método inica el analizador sintactico.
+        
+        @rtype  : Boolean
+        @return : True si es que el proceso a finalizado correctamente
+
+        """
         self.exp_reg()
         return True
 
@@ -40,8 +52,9 @@ class Analizador:
         """
          ExpReg -> Term SubExpReg
         """
-        if DEBUG :
+        if DEBUG:
             print "exp_reg -> Token = ", self.token
+            
         self.term();
         self.sub_exp_reg();
 
@@ -49,8 +62,9 @@ class Analizador:
         """
         SubExpReg -> '|' Term {print '|'} SubExpReg | VACIO
         """
-        if DEBUG :
+        if DEBUG:
             print "sub_exp_reg -> Token = ", self.token
+            
         if self.token != "|":
             return;
 
@@ -63,7 +77,7 @@ class Analizador:
         """
         Term -> Factor SubTerm
         """
-        if DEBUG :
+        if DEBUG:
             print "term -> Token = ", self.token
 
         self.factor();
@@ -73,11 +87,11 @@ class Analizador:
         """
         SubTerm   -> '.' Factor {print '.'} SubTerm | VACIO
         """
-        if DEBUG :
+        if DEBUG:
             print "sub_term-> Token = ", self.token
 
-        if self.token != "." :
-            return ;
+        if self.token != ".":
+            return;
 
         self.match(".");
         self.factor();
@@ -85,84 +99,92 @@ class Analizador:
         self.sub_term();
 
 
-    def factor(self) :
+    def factor(self):
         """
         Factor    -> DefReg   {print DefReg}   (OperUna|VACIO) |
         Factor    -> Alfabeto {print Alfabeto} (OperUna|VACIO) |
         Factor    -> (ExpReg) (OperUna|VACIO)
         """
-        if DEBUG :
+        if DEBUG:
             print "factor -> Token = ", self.token
 
-        if self.token.find(self.definicion_regular) >= 0 :
-            if DEBUG :
+        if self.token.find(self.definicion_regular) >= 0:
+            if DEBUG:
                 print "def_reg -> Token = ", self.token
+                
             self.postfija.append(self.token)
             self.match(self.token)
 
-        elif self.alfabeto.find(self.token) >= 0 :
-            if DEBUG :
+        elif self.alfabeto.find(self.token) >= 0:
+            if DEBUG:
                 print "alfabeto -> Token = ", self.token
+                
             self.postfija.append(self.token)
             self.match(self.token)
 
-        else  :
+        else:
             self.match("(")
             self.exp_reg()
             self.match(")")
 
-        if self.operadores_unarios.find(self.token) >= 0 :
+        if self.operadores_unarios.find(self.token) >= 0:
             self.oper_una()
 
     def oper_una(self):
         """
         OperUna   -> + | * | ? {print OperUna}
         """
-        if DEBUG :
+
+        if DEBUG:
             print "oper_una-> Token = ", self.token
 
-        self.postfija.append(self.token)
-
-        if self.operadores_unarios.find(self.token) < 0 :
+        if self.operadores_unarios.find(self.token) < 0:
             print "ERROR DE SINTAXIS, se esperaba un operador unario"
             exit();
-
+        
+        self.postfija.append(self.token)
         self.match(self.token)
 
-    def match (self, t) :
+    def match (self, t):
         """
         Se encarga de consumir el token y solicitar el siguente token
         si es que no existe un error de sintaxis.
+        
+        @type  t : String
+        @param t : token que se desea consumir.
         """
-        if self.token == t :
-            self.index +=1
-            if  self.index < self.tokens.len() :
+        if self.token == t:
+            self.index += 1
+            if  self.index < self.tokens.len():
                 self.token = self.tokens.get_next_token()
 
-            if DEBUG :
+            if DEBUG:
                 print "MATCH ('" + t + "') -> NEXT('" + self.token + "')\n"
+                
             return;
 
-        else :
-            print "ERROR DE SINTAXIS, inesperado '", t,"' se esperaba '"\
-                , self.token, "'" ; exit()
+        else:
+            print "ERROR DE SINTAXIS, inesperado '", t, "' se esperaba '"\
+                , self.token, "'"; 
+            exit()
 
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
 
-    er = "a*.(b|c)*.d+.a.$HOLA;.b.c.d"
+    er = "a*.(b|c)*.$hola;.a"
 
-    keys =  keys()
+    keys = keys()
     print er
     print "Start.."
     tokens = StringTokenizer (er, keys)
     a = Analizador(tokens, keys);
-    if a.start() :
+    if a.start():
         print "Done.."
 
     postfija = ""
     for c in a.postfija:
-        postfija +=c
-
+    
+        postfija += c
     print postfija
+    print a.postfija
--- string_tokenizer.py
+++ string_tokenizer.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+
 from inf2post import *
 
-class StringTokenizer :
+"""
+:author: Maximiniliano Báez González
+:contact: mxbg.py@gmail.com
+"""
+class StringTokenizer:
     """
     Divide un string en substrings o tokens apartir de los siguentes 
     delimitadores o caracteres:
@@ -10,8 +17,16 @@ class StringTokenizer :
     DEFINICION REGULAR = "$"
     """
     
-    def __init__ (self, expresion, keys) :
+    def __init__ (self, expresion, keys):
         """
+        Se encarga de inicializar los atributos de clase.
+        
+        @type  expresion : String
+        @param expresion : la expresion que se desea separar en tokens
+        
+        @type  keys : Keys
+        @param keys : continen los parametros que se necesitan para 
+                      separar los tokens.
         """
         self.expresion = expresion
         self.tokens = []
@@ -22,63 +37,102 @@ class StringTokenizer :
         self.__tokenizar__(keys);
         
         
-    def __tokenizar__(self, keys) :
+    def __tokenizar__(self, keys):
         """
+        Este método se encarga de separar la cadena en tokens segun 
+        los delimitadores y alfabetos definidos.
+        
+        @type  keys : Keys
+        @param keys : continen los parametros que se necesitan para 
+                      separar los tokens.
         """
         token = ""
         index = 0
         
-        while (index < len(self.expresion)) :
+        while (index < len(self.expresion)):
             
+            #se obtiene el caracter actual
             c = self.expresion[index]
             
-            if self.alfabeto.find(c)>=0 :
-                """
-                """
+            if self.alfabeto.find(c) >= 0:
+                
+                #Si el caracter pertenece al alfabeto se añade el 
+                #caracter al token
                 token += c
                 self.tokens.append(token)
+                #se reinicia el token
                 token = ""
                 
-            elif self.operadores.find(c)>=0 :
-                """
-                """
+            elif self.operadores.find(c) >= 0:
+                
+                #Si el caracter actual es un operador se añade a la 
+                #lista de tokens
                 self.tokens.append(c)
+                #se reinicia el token
                 token = ""
                 
-            elif self.definicion_regular.find(c) >=0 :
-                """
-                """
+            elif self.definicion_regular.find(c) >= 0:
+
+                #Si el caracter posee el signo $ inidica que es un 
+                #definción regular y por lo tanto debe terminar con el 
+                #caracter ';'
                 end = self.expresion.find(";", index, len(self.expresion))
+
+                #Si en es menor a cero, la definicion regular no esta 
+                #definida correctamente, por lo que se emite un error
+                if end < 0 :
+                    print "ERROR: Se esperaba un ';'"
+                #Si la definición regular es correcta se añade a la lista 
+                #de tokens
                 self.tokens.append(self.expresion [(index):end])
+                #Se actualiza el indice para que apunte al final de la
+                #definición regular
                 index = end 
+                #se reinicia el token
                 token = ""
                 
-            else :
-                """
-                """
+            else:
+                #Si el caracter no se encuentra en el alfabeto, no es 
+                #un oprador y tampoco definición regular, se emite un 
+                #mensaje de error
+                
                 print "ERROR: El caracter '" + c + "' no se encuentra definido"
                 exit();
-
-            index +=1
-        print self.tokens
+            #se incrementa el valor del indice para que apunte al siguiente
+            #caracter
+            index += 1
         
         
-        infija = inf2post(self.tokens, keys)
+        #infija = inf2post(self.tokens, keys)
     
     def len(self):
         """
+        Este método se encarga de obtener la cantidad de tokens existentes.
+        
+        @rtype  : Integer
+        @return : la cantidad de tokens
         """
         return len (self.tokens)
         
     def get_next_token(self):
-        """
-        """
         
-        if self.token_index >= self.len() :
+        """
+        Este método obtiene el siguiente token de la lista y actualiza
+        el indice para que apunte al siguiente token.
+        
+        @rtype  : String
+        @return : El siguiente token
+        """
+        #si no existen mas tokens se retorna none
+        if self.token_index >= self.len():
             return None
-            
+        
+        #se obtiene el siguiente token de la lista
         token = self.tokens[self.token_index]
+        
+        #Se actualiza el indice para que apunte al siguiente elemento
         self.token_index += 1
         
+        #retorna el token 
         return token
         
