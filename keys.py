91754f7ca347450b5ea219b168b83ab9c955115e
Maximiliano Báez González <mxbg.py@gmail.com>
2011-10-20 02:47:29 -0300

    Se añadieron las palabras claves como atributos estaticos

--- keys.py
+++ keys.py
@@ -1,8 +1,16 @@
 
-class keys :
+class Keys :
     """
     Conjunto de variables claves del analizador
     """
+    
+    VACIO = "VACIO"
+    OR = "|"
+    PLUS = "+"
+    STAR = "*"
+    CONCAT = "."
+    NONE_OR_ONE = "?"
+    
     def __init__ (self) :
         self.operadores_binarios = "|."
         self.operadores_unarios = "+*?"
