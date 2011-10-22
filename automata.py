78e7e9893258a32933af43d3347989dd376e185b
Maximiliano Báez González <mxbg.py@gmail.com>
2011-10-21 23:12:20 -0300

    Se separa el algoritmo de thompson del modulo automata

--- automata.py
+++ automata.py
@@ -35,7 +35,7 @@ class Estado :
         @return : una copia del estado actual
         """
         estado = Estado()
-        estado.id = self.id + "CP"
+        estado.id = self.id + Keys.COPY_LABEL
         estado.final = self.final
         
         return estado
@@ -160,16 +160,19 @@ class Automata :
         #se crea un automata que sera la copia a retornar
         automata =  Automata()
         
-        for estado in self.estados.values() :
+        for arco in self.arcos :
             #se copia el estado actual
-            copy = estado.copy()
+            src_copy = arco.origen.copy()
+            dest_copy = arco.destino.copy()
             #se añade el estado copia al diccionario de estados del 
             #automata copia.
-            automata.estados[copy.id] = copy
+            automata.estados[src_copy.id] = src_copy
+            automata.estados[dest_copy.id] = dest_copy
+            
         #se obiene el id del estado y se añade la terminación CP que 
         #indica que este estado es una copia.
-        estado_inicial_str = self.estado_inicial.id + "CP"
-        estado_final_str = self.estado_final.id + "CP"
+        estado_inicial_str = self.estado_inicial.id + Keys.COPY_LABEL
+        estado_final_str = self.estado_final.id + Keys.COPY_LABEL
         
         #se establecen los estados inicial y final de automata copia.
         #Los estados inicial y final son obtenidos de la lista de 
@@ -183,8 +186,8 @@ class Automata :
             #por cada arco existente se obtiene los id's de los estados
             #inicial y final y se les concatena el caracter CP para 
             #generar el id de los estados copia
-            origen_str = arco.origen.id + "CP"
-            destino_str = arco.destino.id + "CP"
+            origen_str = arco.origen.id + Keys.COPY_LABEL
+            destino_str = arco.destino.id + Keys.COPY_LABEL
             
             #Se añade los arcos al automata copia, oteniendo las
             #referencias de los estados copias cuyo id esta definido
@@ -209,136 +212,3 @@ class Automata :
         cad = cad[0:-1]
         cad += "" 
         return cad
-        
-class Thompson :
-    
-    def __init__(self, tokens_infija) :
-        self.automatas = []
-        self.tokens_infija = tokens_infija
-        self.index = 0
-        
-    def start(self):
-        for token in self.tokens_infija :
-            if token == Keys.STAR :
-                self.star()
-            elif token == Keys.PLUS :
-                self.plus()
-            elif token == Keys.NONE_OR_ONE :
-                self.none_or_one()
-            elif token == Keys.OR :
-                self._or()
-            elif token == Keys.CONCAT :
-                self.concat()
-            else : 
-                self.single(token)
-            
-    def single(self, simbolo):
-        """
-        
-        @type simbolo  : String 
-        @paran simbolo : simbolo que genra una transición entre estados
-        """
-        automata =  Automata()
-        #Se crean los estados inicial y final
-        estado_inicial = Estado()
-        estado_final = Estado()
-        #Se establecen los estados inicial y final
-        automata.estado_inicial = estado_inicial
-        automata.estado_final = estado_final
-        #Se crea un arco entre los estados incial y final con el simbolo
-        #de transicion correspondiente.
-        automata.add_transicion(estado_inicial, estado_final, simbolo)
-        #se añade  el automata a la pila
-        self.automatas.append(automata)
-        
-    def _or(self) :
-
-        automata =  Automata()
-        
-        afnd_final = self.automatas.pop()
-        afnd_inicial = self.automatas.pop()
-        
-        estado_inicial = Estado()
-        estado_final = Estado()
-        
-        automata.add_estado_inicial(estado_inicial)
-        automata.add_estado_final(estado_final)
-        
-        automata.add_transicion(estado_inicial, afnd_inicial.estado_inicial,\
-                                Keys.VACIO)
-
-        automata.add_transicion(estado_inicial, afnd_final.estado_inicial,\
-                                Keys.VACIO)
-        
-        automata.add_arcos(afnd_inicial.arcos)
-        automata.add_arcos(afnd_final.arcos)
-        
-        automata.add_transicion(afnd_inicial.estado_final,estado_final,\
-                                Keys.VACIO)
-        automata.add_transicion(afnd_final.estado_final, estado_final,\
-                                Keys.VACIO)
-        
-        
-        self.automatas.append(automata)
-        
-        
-    def concat(self) :
-        automata =  Automata()
-        
-        afnd_final = self.automatas.pop()
-        afnd_inicial = self.automatas.pop()
-        
-        automata.add_estado_inicial(afnd_inicial.estado_inicial)
-        automata.add_estado_final(afnd_final.estado_final)
-        #se añade los arcos al automata
-        automata.add_arcos(afnd_inicial.arcos)
-        automata.add_arcos(afnd_final.arcos)
-        #se hace un merge en entre los estados para no generar estados
-        #que no son necesarios
-        afnd_inicial.estado_final.merge(afnd_final.estado_inicial)
-        #se añade el alutomata a la pila
-        self.automatas.append(automata)
-        
-    def none_or_one (self) :
-        
-        self.single(Keys.VACIO)
-        self._or()
-        
-    def plus (self) :
-        automata = self.automatas.pop()
-        
-        self.automatas.append(automata)
-        self.automatas.append(automata.copy())
-
-        self.star()
-        self.concat()
-        
-    def star (self) :
-        automata = self.automatas.pop()
-        
-        self.single(Keys.VACIO)
-        
-        automata.add_transicion(automata.estado_final,automata.\
-                                estado_inicial,Keys.VACIO)
-        self.automatas.append(automata)
-        
-        self.concat()
-        self.single(Keys.VACIO)
-        self.concat()
-        
-        automata = self.automatas.pop()
-        automata.add_transicion(automata.estado_inicial,automata.\
-                                estado_final,Keys.VACIO)
-                                
-        self.automatas.append(automata)
-        
-        
-    def __str__(self) :
-        cad = ""
-        for automata in self.automatas :
-            cad += str(automata) +"\n"
-        
-        cad = cad[0:-1]
-        cad += "" 
-        return cad
-
---
+++ thompson.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python 
+# -*- coding: utf-8 -*- 
+from automata import *
+from keys import *
+        
+class Thompson :
+    
+    def __init__(self, tokens_infija) :
+        self.automatas = []
+        self.tokens_infija = tokens_infija
+        self.index = 0
+        
+    def start(self):
+        for token in self.tokens_infija :
+            if token == Keys.STAR :
+                self.star()
+            elif token == Keys.PLUS :
+                self.plus()
+            elif token == Keys.NONE_OR_ONE :
+                self.none_or_one()
+            elif token == Keys.OR :
+                self._or()
+            elif token == Keys.CONCAT :
+                self.concat()
+            else : 
+                self.single(token)
+        
+        self.automatas[0].estado_final.final = True
+        
+    def single(self, simbolo):
+        """
+        
+        @type simbolo  : String 
+        @paran simbolo : simbolo que genra una transición entre estados
+        """
+        automata =  Automata()
+        #Se crean los estados inicial y final
+        estado_inicial = Estado()
+        estado_final = Estado()
+        #Se establecen los estados inicial y final
+        automata.estado_inicial = estado_inicial
+        automata.estado_final = estado_final
+        #Se crea un arco entre los estados incial y final con el simbolo
+        #de transicion correspondiente.
+        automata.add_transicion(estado_inicial, estado_final, simbolo)
+        #se añade  el automata a la pila
+        self.automatas.append(automata)
+        
+    def _or(self) :
+
+        automata =  Automata()
+        
+        afnd_final = self.automatas.pop()
+        afnd_inicial = self.automatas.pop()
+        
+        estado_inicial = Estado()
+        estado_final = Estado()
+        
+        automata.add_estado_inicial(estado_inicial)
+        automata.add_estado_final(estado_final)
+        
+        automata.add_transicion(estado_inicial, afnd_inicial.estado_inicial,\
+                                Keys.VACIO)
+
+        automata.add_transicion(estado_inicial, afnd_final.estado_inicial,\
+                                Keys.VACIO)
+        
+        automata.add_arcos(afnd_inicial.arcos)
+        automata.add_arcos(afnd_final.arcos)
+        
+        automata.add_transicion(afnd_inicial.estado_final,estado_final,\
+                                Keys.VACIO)
+        automata.add_transicion(afnd_final.estado_final, estado_final,\
+                                Keys.VACIO)
+        
+        
+        self.automatas.append(automata)
+        
+        
+    def concat(self) :
+        automata =  Automata()
+        
+        afnd_final = self.automatas.pop()
+        afnd_inicial = self.automatas.pop()
+        
+        automata.add_estado_inicial(afnd_inicial.estado_inicial)
+        automata.add_estado_final(afnd_final.estado_final)
+        #se añade los arcos al automata
+        automata.add_arcos(afnd_inicial.arcos)
+        automata.add_arcos(afnd_final.arcos)
+        #se hace un merge en entre los estados para no generar estados
+        #que no son necesarios
+        afnd_inicial.estado_final.merge(afnd_final.estado_inicial)
+        #se añade el alutomata a la pila
+        self.automatas.append(automata)
+        
+    def none_or_one (self) :
+        
+        self.single(Keys.VACIO)
+        self._or()
+        
+    def plus (self) :
+        automata = self.automatas.pop()
+        
+        self.automatas.append(automata)
+        self.automatas.append(automata.copy())
+
+        self.star()
+        self.concat()
+        
+    def star (self) :
+        automata = self.automatas.pop()
+        
+        self.single(Keys.VACIO)
+        
+        automata.add_transicion(automata.estado_final,automata.\
+                                estado_inicial,Keys.VACIO)
+        self.automatas.append(automata)
+        
+        self.concat()
+        self.single(Keys.VACIO)
+        self.concat()
+        
+        automata = self.automatas.pop()
+        automata.add_transicion(automata.estado_inicial,automata.\
+                                estado_final,Keys.VACIO)
+                                
+        self.automatas.append(automata)
+        
+        
+    def __str__(self) :
+        cad = ""
+        for automata in self.automatas :
+            cad += str(automata) +"\n"
+        
+        cad = cad[0:-1]
+        cad += "" 
+        return cad
+
