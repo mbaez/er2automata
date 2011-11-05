9d5635c53b8992723c172c65b270fa791f931c23
Maximiliano Báez González <mbaez@mbaez-notebook.(none)>
2011-11-05 17:01:44 -0300

    Se añade la clase que se encarga de manipular las imagenes svg's

---
+++ svg_manager.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from xml.dom.minidom import parse
+
+class SVGManager : 
+
+    document = None
+    nodes = {}
+    
+    def __init__(self, svg_file_name) :
+        """
+        Este metodo se encarga de encarga de inicializar los atributos
+        y parsea el archivo svg para que sea manejable
+        
+        @type svg_file_name  : String
+        @param svg_file_name : nombre del archivo svg
+        """
+        self.svg_file_name = svg_file_name
+        self.document = parse(svg_file_name)
+        self.__load_nodes__()
+    
+    def __load_nodes__(self) : 
+        """
+        Se encarga de prosesar los tags que poseen la siguiente estructura
+        
+        <g id="node2" class="node"> 
+            <title>E4</title>
+            <ellipse fill="none" stroke="black"/>
+            <text>E4</text>
+        </g>
+        
+        Apartir de esta información se generan entradas en un diccionario
+        donde la clave es el texto contenido en <title> </title> y el 
+        es la referncia al atributo de ellipse.
+        
+        ["E4"] -> <ellipse fill="none" stroke="black"/>
+        
+        """
+        elements = self.document.documentElement
+        #se obtiene del svg todos los elementos  que contienen el tag
+        #<g> ... </g>
+        for polygon in elements.getElementsByTagName("g") :
+            #se procesan unicamente aquellos tags que son del tipo nodo
+            if polygon.attributes["class"].value == "node" :
+                #se obiene el tag <title>.. </title>
+                title = polygon.getElementsByTagName("title")[0]
+                #se verifica si el tag posee un valor
+                if title.firstChild !=  None:
+                    #se obtiene el valor contenido dentro del tag
+                    label = str(title.firstChild.data).strip()
+                    #se obtiene el tag <ellipse>... </ellipse>
+                    ellipse = polygon.getElementsByTagName("ellipse")[0]
+                    #se guarda en el diccionario el id del estado y la 
+                    #referencia a la figura
+                    # ["E4"] -> <ellipse fill="none" stroke="black"/>
+                    self.nodes[label] = ellipse
+                
+    def set_node_color(self, node_id, color="#ADD8E6") :
+        """
+        Este método se encarga de reemplazar el fill del la ellipse 
+        identificado por el id node_id y establecer el valor color.
+        
+        [node_id] --> <ellipse fill=color stroke="black
+        
+        @type node_id  : String
+        @param node_id : identificador del estado al cual se desea cambiar
+                         el color.
+        @type color  : String
+        @param color : color que sera asignado al estado identificado por
+                       node_id
+        """
+        if self.nodes.has_key(node_id) :
+            self.nodes[node_id].attributes["fill"].value = color
+
+    
+    def write_svg(self, svg_file_name=None) :
+        """
+        Este metodo se volcar el el svg en un archivo de nombre
+        svg_file, si svg_file no es especificado se sobrescribe el archivo
+        original
+        
+        @type svg_file_name  : String
+        @param svg_file_name : nombre del archivo en el cual se va 
+                               a escribir el svg, si no se especifica
+                               el nombre sobrescribe el archivo original
+        
+        """
+        if svg_file_name == None : 
+            svg_file_name =  self.svg_file_name
+        #se el archivo, si no existe se crea 
+        source = open(svg_file_name, 'w')
+        # se escribe en el archivo el contenido del documento xml actual
+        source.write(self.document.toprettyxml())
+        #se cierra el archivo
+        source.close()
+
+svg = SVGManager("afd.svg")
+svg.set_node_color("E4")
+svg.write_svg()
